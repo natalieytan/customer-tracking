db663e43760a9b71ed9546d296c564c28ad04060
Natalie Tan <natalieytan@gmail.com>
2024-10-17 10:13:20 +1100

    disable ESlint

--- src/app/hello/route.ts
+++ src/app/hello/route.ts
@@ -1,6 +1,7 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 export const runtime = 'nodejs';
  
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 export function GET(_request: Request) {
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
